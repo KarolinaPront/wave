40627ee9f96436e295cd5bc722a554079832ddf2
KarolinaPront <166846553+KarolinaPront@users.noreply.github.com>
2024-04-28 20:59:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.animate__backInRight');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
